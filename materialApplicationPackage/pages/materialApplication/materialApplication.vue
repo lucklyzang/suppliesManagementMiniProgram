06955e7ec77ccc9103ba5cabe3a4a68df8d80108
lucklyzang <luckyzangvip@163.com>
2026-03-06 17:28:37 +0800

    添加产品功能逻辑开发

--- materialApplicationPackage/pages/materialApplication/materialApplication.vue
+++ materialApplicationPackage/pages/materialApplication/materialApplication.vue
@@ -22,7 +22,10 @@
 				</view>
 			</view>
 			<view class="content-center">
-				<view class="product-list" v-for="(item,index) in materialList" :key="item.productName">
+				<view class="empty-info" v-if="chooseMaterialList.length == 0">
+					<u-empty text="暂无产品" mode="list"></u-empty>
+				</view>
+				<view class="product-list" v-for="(item,index) in chooseMaterialList" :key="item.productName">
 					<view class="product-left">
 						<image :src="item.productImage" mode="widthFix"></image>
 					</view>
@@ -55,7 +58,7 @@
 						</view>
 						<view class="product-total-price">
 							<text>￥</text>
-							<text>{{ item.totalPrice }}</text>
+							<text>{{ item.showTotalPrice }}</text>
 						</view>
 					</view>
 				</view>
@@ -63,14 +66,14 @@
 			<view class="total-prices">
 				<view class="total-prices-right">
 					<text>合计:</text>
-					<text>{{ `￥${allChooseProductPrice}` }}</text>
+					<text>{{ `￥${formatPrice(allChooseProductPrice)}` }}</text>
 				</view>
 			</view>
 			<view class="content-bottom">
 				<view class="content-bottom-left">
 					<text>保存</text>
 				</view>
-				<view class="content-bottom-right">
+				<view class="content-bottom-right" @click="materialApplicationSubmitEvent">
 					<text>保存并继续</text>
 				</view>
 			</view>
@@ -85,15 +88,55 @@
 								placeholder="请输入搜索关键词"
 								border="none"
 								v-model="searchValue"
-								@change="searchChange"
 							></u--input>
 						</view>
-						<view class="search-box">
+						<view class="search-box" @click="searchChange">
 							<u-icon name="search" color="#888888" size="20"></u-icon>
 						</view>
 					</view>
 					<view class="product-popup-center">
-							
+							<view class="empty-info" v-if="materialList.length == 0">
+								<u-empty text="暂无产品" mode="list"></u-empty>
+							</view>
+							<view class="add-product-popup-list" v-for="(item,index) in materialList" :key="item.productName" @click="addProductItemEvent(item,index)">
+								<view class="add-product-popup-top">
+									<view class="add-product-popup-top-left">
+										<image :src="item.productImage" mode="widthFix"></image>
+										<view>
+											<text>
+												{{ item.productName }}
+											</text>
+										</view>
+										<view>
+											<text>
+												{{ `,${item.specification},` }}
+											</text>
+										</view>
+										<view>
+											<text>￥</text>
+											<text>
+												{{ item.unitPrice }}
+											</text>
+											<text>
+												{{ `/${item.unit}` }}
+											</text>
+										</view>
+									</view>
+									<view class="add-product-popup-top-right" :class="{'popupTopRightStyle':item.checked == true, 'popupTopRightDisabledStyle':item.disabled == true}">
+										<u-icon v-if="item.checked == true" name="checkbox-mark" color="#fff" size="18"></u-icon>
+									</view>
+								</view>
+								<view class="add-product-popup-bottom" @click.stop="handleClick">
+									<text>*</text>
+									<text>请输入数量:</text>
+									<u--input 
+									   v-model="item.quantity"
+										 :disabled="item.disabled"
+									   placeholder=""
+									   border="none"
+									 ></u--input>
+								</view>
+							</view>
 					</view>
 					<view class="product-popup-bottom">
 							<view class="cancel-box" @click="productChoosePopupCloseEvent">
@@ -111,7 +154,7 @@
 <script>
 import { mapGetters, mapMutations } from "vuex";
 import { repairsDetails, repairsImage } from "@/api/project.js";
-import { setCache, removeAllLocalStorage, } from '@/common/js/utils'
+import { setCache, removeAllLocalStorage, deepClone} from '@/common/js/utils'
 import navBar from "@/components/zhouWei-navBar"
 export default {
   components: {
@@ -124,26 +167,30 @@ export default {
 			tierNum: 0,
 			searchValue: '',
 			allChooseProductPrice: 0,
+			originalMaterialList: [],
 			materialList: [
 				{
 					productName: '洗手液',
 					specification: '500ML',
 					unit: '瓶',
 					unitPrice: '4.5',
-					totalPrice: 0,
 					quantity: 0,
-					productImage: require('@/static/img/home-apply-icon.png')
+					checked: false,
+					disabled: false,
+					productImage: require('@/static/img/basic-message.png')
 				},
 				{
 					productName: '一次性垫',
 					specification: '90*40cm',
 					unit: '包',
 					unitPrice: '8.3',
-					totalPrice: 0,
 					quantity: 0,
-					productImage: require('@/static/img/home-apply-icon.png')
+					checked: false,
+					disabled: false,
+					productImage: require('@/static/img/basic-message.png')
 				}
 			],
+			chooseMaterialList: [],
 			productChooseShow: false
     }
   },
@@ -154,7 +201,8 @@ export default {
 			this.tierNum = 1
 		} else {
 			this.tierNum = pages.length;
-		}
+		};
+		this.originalMaterialList = deepClone(this.materialList);
 	},
 	
   watch: {},
@@ -192,10 +240,10 @@ export default {
      backTo () {
      	uni.navigateBack()
      },
-		 
+		 handleClick () {},
 		 // 求和函数(计算所有添加产品总价格)
 		 reduceTotal() {
-			 let targetMsg = this.materialList.filter((item) => {
+			 let targetMsg = this.chooseMaterialList.filter((item) => {
 				 return item.quantity > 0
 			 });
 			 this.allChooseProductPrice = targetMsg.reduce((accumulator, currentValue) => {
@@ -205,19 +253,38 @@ export default {
 		 
 		 // 保留两位小数，返回数字类型，修复精度问题
 		 formatPrice(num) {
-		   if (typeof num !== 'number') return 0;
-		   return Number(num.toFixed(2));
+		   if (typeof num !== 'number' || isNaN(num)) return "0.00";
+			 const value = Math.round(num * 100) / 100;
+			 return value.toFixed(2);
 		 },
 		 
 		 // 产品步进器change事件
 		 productNumberBoxChange(item,index,val) {
-			 item['totalPrice'] = this.formatPrice(item['unitPrice'] * val['value']);
+			 if (val['value'] == 0) {
+				 this.chooseMaterialList.splice(index,1);
+			 };
+			 item['showTotalPrice'] = this.formatPrice(item['unitPrice'] * val['value']);
+			 item['totalPrice'] = item['unitPrice'] * val['value'];
 			 this.reduceTotal();
 		 },
 		 
 		 // 添加产品事件
 		 addProductEvent () {
-			 this.productChooseShow = true;
+				this.productChooseShow = true;
+				this.searchValue = '';
+				for (let item of this.materialList) {
+					 // 添加过的产品不允许再次添加
+					 let isExist = this.chooseMaterialList.filter((innerItem) => { return innerItem.productName == item.productName});
+					 if (isExist.length > 0) {
+						 item['checked'] = true;
+						 item['disabled'] = true;
+						 item['quantity'] = isExist[0]['quantity'];
+					 } else {
+						 item['checked'] = false;
+						 item['disabled'] = false;
+						 item['quantity'] = 0;
+					 }
+				}
 		 },
 		 
 		 // 添加产品弹框关闭事件
@@ -226,11 +293,52 @@ export default {
 		 },
 		 
 		 // 搜索框值改变事件
-		 searchChange () {},
+		 searchChange () {
+			 if (this.searchValue === '') {
+				 this.materialList = this.originalMaterialList
+			 };
+			 this.materialList = this.materialList.filter((item) => { return item['productName'].indexOf(this.searchValue) != -1 })
+		 },
 		 
 		 // 添加确认事件
 		 addSureEvent () {
+			 let count = this.materialList.some((item)=> {return item.checked == true && !item.disabled});
+			 if (!count) {
+				 this.$refs.uToast.show({
+				 	message: '至少要选择一种产品',
+				 	position: 'center'
+				});
+				return;
+			 };
 			 this.productChooseShow = false;
+			 let temporaryMaterialList = this.materialList.filter((item) => { return item['checked'] === true && !item.disabled });
+			 for (let item of temporaryMaterialList) {
+				this.chooseMaterialList.push({
+					productName: item['productName'],
+					specification: item['specification'],
+					unit: item['unit'],
+					unitPrice: item['unitPrice'],
+					quantity: item['quantity'],
+					totalPrice: 0,
+					showTotalPrice: '0.00',
+					productImage: item['productImage']
+				}) 
+			 };
+			 this.reduceTotal();
+		 },
+		 
+		 // 添加产品弹框内产品项点击事件
+		 addProductItemEvent(item,index) {
+			 if (!item.disabled) {
+				 item.checked = !item.checked;
+			 }
+		 },
+		 
+		 // 物资申请提交事件
+		 materialApplicationSubmitEvent () {
+			 uni.navigateTo({
+			 	url: '/materialApplicationPackage/pages/materialApplicationSubmit/materialApplicationSubmit'
+			 })
 		 }
 	}
 };
@@ -261,7 +369,7 @@ page {
 			.u-popup__content {
 				.u-popup__content__close {
 					.u-icon__icon {
-						font-size: 20px !important;
+						font-size: 22px !important;
 						color: #101010 !important;
 					}
 				}
@@ -303,7 +411,90 @@ page {
 			};
 			.product-popup-center {
 				flex: 1;
+				padding: 6px 4px;
+				box-sizing: border-box;
 				overflow: auto;
+				position: relative;
+				.empty-info {
+					width: 100px;
+					height: 120px;
+					position: absolute;
+					top: 0;
+					left: 0;
+					bottom: 0;
+					right: 0;
+					margin: auto;
+					z-index: 100;
+				};
+				.add-product-popup-list {
+					margin-bottom: 10px;
+					.add-product-popup-top {
+						display: flex;
+						align-items: center;
+						.add-product-popup-top-left {
+							margin-right: 10px;
+							flex: 1;
+							width: 0;
+							display: flex;
+							align-items: center;
+							>image {
+								width: 36px;
+								height: 36px;
+								margin-right: 4px;
+							};
+							>view {
+								text {
+									font-size: 14px;
+									color: #101010;
+								};
+								&:nth-child(3) {
+									margin: 0 4px;
+								}
+							}
+						};
+						.add-product-popup-top-right {
+							width: 24px;
+							height: 24px;
+							display: flex;
+							align-items: center;
+							justify-content: center;
+							border: 1px solid #BBBBBB;
+						};
+						.popupTopRightStyle {
+							background: #11D183 !important;
+						};
+						.popupTopRightDisabledStyle {
+							opacity:.5 !important;
+						}
+					};
+					.add-product-popup-bottom {
+						display: flex;
+						justify-content: flex-end;
+						align-items: center;
+						margin-top: 6px;
+						>text {
+							&:nth-child(1) {
+								font-size: 14px;
+								color: #F44E23;
+							};
+							&:nth-child(2) {
+								font-size: 14px;
+								color: #101010;
+								margin-right: 4px;
+							}
+						};
+						.u-input {
+							width: 60px;
+							height: 25px;
+							flex: none !important;
+							border-radius: 5px;
+							padding: 6px !important;
+							box-sizing: border-box;
+							color: #11D183;
+							border: 1px solid #11D183;
+						}
+					}
+				}
 			};
 			.product-popup-bottom {
 				height: 80px;
@@ -311,7 +502,7 @@ page {
 				align-items: center;
 				justify-content: space-between;
 				.cancel-box {
-					width: 40%;
+					width: 45%;
 					height: 48px;
 					display: flex;
 					align-items: center;
@@ -324,7 +515,7 @@ page {
 					}
 				};
 				.add-box {
-					width: 40%;
+					width: 45%;
 					height: 48px;
 					display: flex;
 					align-items: center;
@@ -396,6 +587,18 @@ page {
 			 border-radius: 6px;
 			 background: #F0F2FE;
 			 overflow: auto;
+			 position: relative;
+			 .empty-info {
+					width: 100px;
+					height: 120px;
+					position: absolute;
+					top: 0;
+					left: 0;
+					bottom: 0;
+					right: 0;
+					margin: auto;
+					z-index: 100;
+			 };
 			 .product-list {
 				 padding: 10px 4px;
 				 box-sizing: border-box;
---
+++ materialApplicationPackage/pages/materialApplicationSubmit/materialApplicationSubmit.vue
@@ -0,0 +1,454 @@
+<template>
+	<view class="content-box">
+		<u-transition :show="showLoadingHint" mode="fade-down">
+			<view class="loading-box" v-if="showLoadingHint">
+				<u-loading-icon :show="showLoadingHint" :text="infoText" size="18" textSize="16"></u-loading-icon>
+			</view>
+		</u-transition>
+		<light-hint ref="alertToast"></light-hint>
+		<view class="top-background-area" :style="{ 'height': statusBarHeight + navigationBarHeight + 5 + 'px' }"></view>
+		<u-toast ref="uToast"></u-toast>
+		<!-- 医院 -->
+		<view class="transport-rice-box" v-if="showTaskType">
+			<ScrollSelection v-model="showHospital" :pickerValues="hospitalDefaultIndex" :isShowSearch="true" :columns="hospitalOption" title="任务类型" @sure="hospitalSureEvent" @cancel="hospitalCancelEvent" @close="hospitalCloseEvent" />
+		</view>
+		<view class="nav">
+			<nav-bar :home="false" :isShowBackText="true" backState='3000' fontColor="#FFF" bgColor="none" title="产品申领" @backClick="backTo">
+			</nav-bar>
+		</view>
+		<view class="content-box-inner">
+			<view class="message-box">
+				<view class="select-box">
+					<view class="select-box-left">
+						<text>*</text>
+						<text>医院</text>
+					</view>
+					<view class="select-box-center">
+							<text>{{ currentHospital }}</text>
+					</view>
+					<view class="select-box-right" @click="showHospital = true">
+						<u-icon name="arrow-right" color="#101010" size="20" /></u-icon>
+					</view>
+				</view>
+				<view class="select-box address-box">
+					<view class="select-box-left">
+						<text>*</text>
+						<text>送货地址</text>
+					</view>
+					<view class="select-box-right">
+						 <u--input
+								v-model="deliveryAddress"
+						    placeholder=""
+						    border="none"
+						    clearable
+						  ></u--input>
+					</view>
+				</view>
+				<view class="select-box">
+					<view class="select-box-left">
+						<text>*</text>
+						<text>交货日期</text>
+					</view>
+					<view class="select-box-center">
+							<text>{{ deliveryDate }}</text>
+					</view>
+					<view class="select-box-right" @click="showDeliveryDate = true">
+						<u-icon name="calendar" color="#101010" size="24" /></u-icon>
+					</view>
+				</view>
+				<view class="transport-type">
+					<view class="transport-type-left">
+						<text>备注:</text>
+					</view>
+					<view class="transport-type-right">
+						<u--textarea v-model="taskDescribe" placeholder="请输入备注" border="none"></u--textarea>
+					</view>
+				</view>
+			</view>
+		</view>
+		<view class="btn-box">
+			<text class="operate-two" @click="sureEvent">提交</text>
+		</view>
+		<!-- 送货时间选择框 -->
+		 <u-datetime-picker
+			@cancel="showDeliveryDate = false" 
+			@confirm="deliveryDateSure"
+			:show="showDeliveryDate"
+			v-model="deliveryDatevalue"
+			mode="date"
+		></u-datetime-picker>
+	</view>
+</template>
+
+<script>
+	import {
+		mapGetters,
+		mapMutations
+	} from 'vuex'
+	import {
+		queryTaskType,
+		queryAllDestination,
+		departmentRoom,
+		reportProblem,
+		getRemarks,
+		querySpace
+	} from '@/api/project.js'
+	import navBar from "@/components/zhouWei-navBar"
+	import { setCache,removeAllLocalStorage, getDate } from '@/common/js/utils'
+	import SOtime from '@/common/js/utils/SOtime.js';
+	import _ from 'lodash'
+	import ScrollSelection from "@/components/scrollSelection/scrollSelection";
+	import LightHint from "@/components/light-hint/light-hint.vue";
+	export default {
+		components: {
+			navBar,
+			ScrollSelection,
+			LightHint
+		},
+		data() {
+			return {
+				showLoadingHint: false,
+				infoText: '加载中···',
+				valueName: 0,
+				loadingShow: false,
+				searchValue: '',
+				taskDescribe: '',
+				deliveryAddress: '',
+				deliveryDate: getDate(),
+				deliveryDatevalue: this.normalizeTimestamp(),
+				showDeliveryDate: false,
+				hospitalOption: [],
+				hospitalDefaultIndex: [0],
+				showHospital: false,
+				currentTaskType: '请选择'
+			}
+		},
+		computed: {
+			...mapGetters([
+				'userInfo',
+				'statusBarHeight',
+				'navigationBarHeight',
+				"templateType"
+			]),
+			userName() {
+				return this.userInfo['name']
+			},
+			proName () {
+			  return this.userInfo['proName']
+			},
+			proId() {
+				return this.userInfo['proId']
+			},
+			workerId() {
+				return this.userInfo['user']['id']
+			},
+			depId() {
+				return this.userInfo['depId'] === null ? '' : this.userInfo['depId']
+			},
+			depName() {
+				return this.userInfo['depName'] === null ? '' : this.userInfo['depName']
+			},
+			userAccount() {
+				return this.userInfo['userName']
+			}
+		},
+		onLoad () {
+		},
+		methods: {
+			...mapMutations([
+			]),
+			
+			// 顶部导航返回事件
+			backTo () {
+				uni.navigateBack()
+			},
+			
+			// 将时间戳转换为当天的 00:00:00
+			normalizeTimestamp () {
+			  const date = new Date();
+			  date.setHours(0, 0, 0, 0);
+			  return Number(date)
+			},
+			
+				// 医院下拉选择框确认事件
+				hospitalSureEvent (val,value,id) {
+				if (val) {
+					this.hospitalDefaultIndex = [id];
+					this.currentHospital =  val
+				} else {
+					this.currentHospital = '请选择'
+				};
+				this.showHospital = false
+				},
+
+				// 医院下拉选择框取消事件
+				hospitalCancelEvent () {
+					this.showHospital = false
+				},
+
+				// 医院下拉选择框关闭事件
+				hospitalCloseEvent () {
+					this.showHospital = false
+				},
+				
+				// 送货时间选择框确认事件
+				deliveryDateSure (e) {
+					this.deliveryDate = SOtime.time8(e.value);
+					this.showDeliveryDate = false
+				},
+
+				// 确认事件
+				sureEvent () {
+					// 任务类型不能为空
+					if (this.currentTaskType == '请选择') {
+						this.$refs.uToast.show({
+							message: '任务类型不能为空',
+							position: 'center'
+						});
+						return
+					};
+					// 创建工程维保任务
+					let temporaryMessage = {
+						typeId: this.taskTypeOption.filter((item) => { return item['text'] == this.currentTaskType})[0]['value'], // 任务类型id
+						typeName: this.currentTaskType, // 任务类型名称
+						depName: this.currentGoalDepartment == '请选择' ? '' : this.currentGoalDepartment, //科室名称
+						depId: this.currentGoalDepartment == '请选择' ? '' : this.goalDepartmentOption.filter((item) => { return item['text'] == this.currentGoalDepartment})[0]['value'], // 目的科室id
+						spaceId: this.currentGoalSpaces == '请选择' ? '' : this.goalSpacesOption.filter((item) => { return item['text'] == this.currentGoalSpaces})[0]['value'], //目的房间id
+						space: this.currentGoalSpaces == '请选择' ? '' : this.currentGoalSpaces, //目的房间名称
+						priority: this.priorityRadioValue,
+						taskDesc: this.taskDescribe, //任务描述
+						proId: this.proId,
+						workerId: this.workerId,
+						workerName: this.userAccount,
+						flag: 1,
+						images: this.imgArr, // 问题图片信息 非必输
+						createType: 3,
+						flag: this.isMedicalMan ? 1 : 0 // 上报人类型，0-维修人员，1-医护人员
+					};
+					this.postGenerateRepairsTask(temporaryMessage)
+				},
+
+				// 生成工程任务
+				postGenerateRepairsTask (data) {
+					this.infoText = '提交中···';
+					this.showLoadingHint = true;
+					reportProblem(data).then((res) => {
+						this.showLoadingHint = false;
+						if (res && res.data.code == 200) {
+							this.$refs.alertToast.show({
+								type: 'success',
+								message: '提交成功!',
+								isShow: true
+							});
+							setTimeout(() => {
+								this.backTo();
+							},2000);
+							this.resetEvent();
+						} else {
+							this.$refs.alertToast.show({
+								type: 'error',
+								message: `${res.data.msg}!`,
+								isShow: true
+							})
+						}
+					})
+					.catch((err) => {
+						this.showLoadingHint = false;
+						this.$refs.alertToast.show({
+							type: 'error',
+							message: `${err}!`,
+							isShow: true
+						})
+					})
+				}
+		}
+	}
+</script>
+
+<style lang="scss">
+	@import "~@/common/stylus/variable.scss";
+	page {
+		width: 100%;
+		height: 100%;
+	};
+	.content-box {
+		@include content-wrapper;
+		height: 100vh !important;
+		box-sizing: border-box;
+		background: #fff;
+		::v-deep .u-popup {
+			flex: none !important
+		};
+		::v-deep .u-loading-icon {
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%,-50%);
+			z-index: 200000;
+		};
+		::v-deep .u-transition {
+			z-index: 100000 !important;
+		};
+		.top-background-area {
+			width: 100%;
+			background: #7BE9A0;
+			position: absolute;
+			top: 0;
+			left: 0;
+			z-index: 10
+		};
+		.nav {
+			width: 100%;
+		};
+		.content-box-inner {
+				width: 100%;
+				height: 0;
+				background: #fff;
+				z-index: 10;
+				flex: 1;
+				overflow: auto;
+				padding: 0 0 10px 0;
+				box-sizing: border-box;
+				display: flex;
+				flex-direction: column;
+				::v-deep .u-empty {
+					position: absolute;
+					top: 50%;
+					left: 50%;
+					transform: translate(-50%,-50%)
+				};
+				.message-box {
+					height: 100%;
+					width: 100%;
+					overflow: scroll;
+					.message-one {
+						width: 100%;
+						padding: 10px 6px 10px 10px;
+						box-sizing: border-box;
+						background: #fff;
+						display: flex;
+						align-items: center;
+						justify-content: space-between;
+						font-size: 14px;
+						margin-top: 6px;
+						.message-one-left {
+							width: 20%;
+							color: #9E9E9A;
+						};
+						.message-one-right {
+							flex: 1;
+							/deep/ .u-radio-group {
+								display: flex;
+								justify-content: space-between;
+								.u-radio {
+									flex: 1 0 auto !important;
+									justify-content: center;
+									.u-radio__label {
+										margin-right: 9px;
+									}
+								}
+							}
+						}
+					};
+					.select-box {
+						width: 100%;
+						padding: 8px 6px 8px 10px;
+						box-sizing: border-box;
+						background: #fff;
+						display: flex;
+						align-items: center;
+						justify-content: center;
+						font-size: 14px;
+						margin-top: 6px;
+						.select-box-left {
+							padding-right: 10px;
+							box-sizing: border-box;
+							>text {
+								&:nth-child(1) {
+									color: red;
+									margin-right: 2px;
+								};
+								&:nth-child(2) {
+									color: #9E9E9A;
+									padding-right: 6px;
+									box-sizing: border-box
+								}
+							}
+						};
+						.select-box-center {
+								flex: 1;
+								>text {
+									color: #101010;
+									text-align: left;
+									display: inline-block;
+									width: 100%;
+									@include no-wrap();
+								}
+						};
+						.select-box-right {
+							align-items: center;
+							display: flex;
+						}
+					};
+					.address-box {
+						.select-box-right {
+							align-items: center;
+							display: flex;
+							flex: 1;
+						}
+					};
+					.transport-type {
+						width: 100%;
+						padding: 10px 6px 10px 10px;
+						box-sizing: border-box;
+						background: #fff;
+						display: flex;
+						flex-direction: column;
+						margin-top: 6px;
+						font-size: 14px;
+						.transport-type-left {
+							margin-bottom: 6px;
+							>text {
+								&:nth-child(1) {
+									color: #101010;
+								}
+							}
+						};
+						.transport-type-right {
+							display: flex;
+							flex-wrap: wrap;
+							/deep/ .u-textarea {
+								flex: 1;
+								padding:10px 6px 10px 6px;
+								color: #101010;
+								border: 1px solid #888888;
+							}
+						}
+					}
+				}
+		};
+		.btn-box {
+			width: 100%;
+			background: #F8F8F8;
+			height: 100px;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			>text {
+				width: 35%;
+				display: inline-block;
+				height: 45px;
+				font-size: 14px;
+				line-height: 45px;
+				background: #fff;
+				text-align: center;
+				border-radius: 4px;
+				&:nth-child(1) {
+					color: #fff;
+					box-sizing: border-box;
+					background: #11D183;
+				}
+			}
+		}
+	}
+</style>
